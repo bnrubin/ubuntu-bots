2097ece8c5925bc7530d625ef7d18eb4d7107731
Terence Simpson <tsimpson@ubuntu.com>
2010-04-30 13:50:43 +0100

    Remove all of the user updating code from IRCLogin, it's just broken and wrong Remove IRCLogin/lp.py

--- IRCLogin/plugin.py
+++ IRCLogin/plugin.py
@@ -20,10 +20,7 @@ import supybot.ircmsgs as ircmsgs
 import supybot.callbacks as callbacks
 import supybot.ircdb as ircdb
 import supybot.conf as conf
-import supybot.schedule as schedule
 import random, os, sys
-from cPickle import Unpickler, Pickler
-import lp
 
 def checkCapab(msg, irc):
     try:
@@ -59,94 +56,6 @@ launchpad"""
             self._irc.queueMsg(ircmsgs.IrcMsg('CAP REQ -IDENTIFY-MSG')) # Disable identify-msg
             self._irc._Freenode_capabed = self._irc._Freenode_capabed_notices = False
 
-    def updateusers(self, irc, msg, args):
-        """Takes no arguments
-
-        Update the user database from Launchpad"""
-        def writePickle(uf, user2nick, nick2user):
-            global self
-            try:
-                fd = open(uf, 'wb')
-            except IOError, e:
-                self.log.error("Could not write to %s (%s)" % (uf, e))
-                return
-            except Exception, e:
-                self.log.error("Unknown error while opening %s for writing:\n%s" % (uf, e))
-                return
-            pi = Pickler(fd, 2)
-            pi.dump((user2nick, nick2user))
-
-        if not checkCapab(msg, irc):
-            return
-        uf = self.registryValue('UserList')
-        if not uf:
-            self.log.info('no UserList config set')
-            irc.error('No UserList config set')
-            return
-        if not os.path.exists(uf):
-            self.log.info('Creating initial userlist')
-        else:
-            self.log.info('Updating userlist')
-        irc.reply('Running...')
-        user2nick = {}
-        nick2user = {}
-        users = lp.getUsers(self.registryValue("teamname"))
-        for user in users:
-            lpuser = lp.getIRCNick(user, False)
-            if not lpuser:
-                lpuser = [user]
-            user2nick[user] = lpuser
-            for nick in lpuser:
-                nick2user[nick] = user
-        writePickle(uf, user2nick, nick2user)
-        self.loadUsers()
-        irc.reply('updateusers run complete')
-    updateusers = wrap(updateusers)
-
-    def loadUsers(self):
-        uf = self.registryValue('UserList')
-        if not uf or not os.path.exists(uf) or not os.access(uf, os.R_OK):
-            self.log.info('Not loading non-existant userlist')
-            return
-        fd = open(uf, 'rb')
-        up = Unpickler(fd)
-        (self.user2nick, self.nick2user) = up.load()
-        nick2user = self.nick2user
-        user2nick = self.user2nick
-        self.knownusers = [i.lower() for i in nick2user.keys()]
-        allusers = [u.name.lower() for u in ircdb.users.itervalues()]
-        to_add = [x for x in self.knownusers if x not in allusers]
-        for a in to_add:
-            self.log.info("Adding %s" % a)
-            user = ircdb.users.newUser()
-            user.name = a
-            rp = ''
-            chars = '''1234567890-=~!@#$%^&*()_qwertyuiop[]QWERTYUIOP{}}|asdfghjkl;ASDFGHJKL:zxcvbnm,./ZXCVBNM<>?'''
-            for i in range(16):
-                rp += chars[random.randint(1,len(chars))-1]
-            user.setPassword(rp)
-        bu = []
-        for u in nick2user.keys():
-            try:
-                user = ircdb.users.getUser(u.lower())
-                if not 'bantracker' in user.capabilities:
-                    user.addCapability('bantracker')
-            except Exception, e:
-                bu.append("%s (%s)" % (u, e))
-                pass
-        if bu:
-            self.log.info("Could not add users %s" % bu)
-
-    def reloadusers(self, irc, msg, args):
-        """Takes no arguments
-
-        Read the user database and add the users in it"""
-        if not checkCapab(msg, irc):
-            return
-        self.loadUsers()
-        irc.replySuccess()
-    reloadusers = wrap(reloadusers)
-
     def login(self, irc, msg, args):
         """takes no arguments
 
@@ -158,26 +67,17 @@ launchpad"""
         nick = msg.nick.lower()
         user = None
         try:
-            user = self.nick2user.get(nick, None)
-            if user:
-                user = ircdb.users.getUser(user)
+            user = ircdb.users.getUser(msg.prefix)
         except:
-            user = None
-            pass
-        if not user:
             try:
                 user = ircdb.users.getUser(msg.prefix)
             except:
-                self.loadUsers()
-                try:
-                    user = ircdb.users.getUser(msg.prefix)
-                except:
-                    for (id, obj) in ircdb.users.users.iteritems():
-                        if obj.name.lower() == nick:
-                            user =  obj
-                    if not user:
-                        irc.error(conf.supybot.replies.incorrectAuthentication())
-                        return
+                for (id, obj) in ircdb.users.users.iteritems():
+                    if obj.name.lower() == nick:
+                        user =  obj
+                if not user:
+                    irc.error(conf.supybot.replies.incorrectAuthentication())
+                    return
         try:
             user.addAuth(msg.prefix)
         except:
@@ -211,7 +111,6 @@ launchpad"""
         try:
             user = ircdb.users.getUser(msg.prefix)
             if user.checkHostmask(msg.prefix):
-                #self.log.info("%s is a known user: %r" % (msg.prefix, user))
                 return
         except:
             pass
